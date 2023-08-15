4434bd1e0022a9d918918dd706750d7dae0b882f
Kalil Bego <kalil.bego@safra.com.br>
2023-08-14 21:57:55 -0300

    Adicionando ação no botão de sair na página Profile

--- Profile/Profile.js
+++ Profile/Profile.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { ExitButton, ExitText, MainView } from './Style';
 
-export default function Profile() {
+export default function Profile({ navigation }) {
     return (
         <MainView>
-            <ExitButton>
+            <ExitButton
+                onPress={() => {
+                    navigation.popToTop();
+                }}
+            >
                 <ExitText>Sair</ExitText>
             </ExitButton>
         </MainView>
