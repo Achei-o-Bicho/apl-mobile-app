caae20ae607e70db246f733e3120adc5f1c8649d
Victor Barbosa <victor.barbosa@bandtec.com.br>
2023-08-15 00:24:32 -0300

    wip: pet registration screen

--- CreatePet/CreatePet.js
+++ CreatePet/CreatePet.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { AddNewPet, MainView, NewPetView, SubtitleNewPet, TitleNewPet, ContinueButton, TextContinue, GenderPet } from './Style';
 import { TextInput, RadioButton } from 'react-native-paper';
 import { TextInputMask } from 'react-native-masked-text';
+import { Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function CreatePet() {
     const [step, setStep] = React.useState(0);
@@ -9,9 +11,37 @@ export default function CreatePet() {
     const [birthday, setBirthday] = React.useState('');
     const [selectedOption, setSelectedOption] = React.useState('male');
     const [description, setDescription] = React.useState('');
+    const [imageUri, setImageUri] = React.useState(null);
 
     const handleStep = (step) => {
         setStep(step);
+    };
+
+    const handleImageUpload = async () => {
+        const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if (permissionResult.granted === false) {
+            alert("Permissão para acessar a galeria é necessária!");
+            return;
+        }
+
+        const imageResult = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            quality: 1,
+        });
+
+        if (!imageResult.canceled) {
+            const formData = new FormData();
+            formData.append('image', {
+                uri: imageResult.assets[0].uri,
+                type: 'image/jpeg',
+                name: 'upload.jpg',
+            });
+
+            console.log(formData);
+
+        }
     };
 
     return (
@@ -115,6 +145,9 @@ export default function CreatePet() {
                         <TitleNewPet>
                             Escolha até 4 fotos do seu pet
                         </TitleNewPet>
+
+                        <Button title="Selecionar Imagem" onPress={handleImageUpload} />
+                        {imageUri && <Image source={{ uri: imageUri }} style={{ width: 500, height: 500 }} resizeMode="cover" />}
 
                         <ContinueButton onPress={() => handleStep(4)}>
                             <TextContinue>
