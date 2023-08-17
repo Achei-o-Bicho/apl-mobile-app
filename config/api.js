9e3d54841528f4ce90266b3340cd02b15f2b510b
Victor Barbosa <victor.barbosa@bandtec.com.br>
2023-08-17 10:05:54 -0300

    add: parameter timeout in get and post method

--- config/api.js
+++ config/api.js
@@ -2,9 +2,16 @@ import config from './configEnviroment';
 
 const BASE_URL = config.API_URL;
 
-export const apiGet = async (endpoint) => {
+export const apiGet = async (endpoint, timeout = 5000) => {
     try {
-        const response = await fetch(`${BASE_URL}/${endpoint}`);
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), timeout);
+
+        const response = await fetch(`${BASE_URL}/${endpoint}`, {
+            signal: controller.signal
+        });
+
+        clearTimeout(timeoutId);
 
         if (!response.ok) {
             throw new Error('Erro de requisição');
@@ -17,15 +24,21 @@ export const apiGet = async (endpoint) => {
     }
 };
 
-export const apiPost = async (endpoint, data) => {
+export const apiPost = async (endpoint, data, timeout = 5000) => {
     try {
+        const controller = new AbortController();
+        const timeoutId = setTimeout(() => controller.abort(), timeout);
+
         const response = await fetch(`${BASE_URL}/${endpoint}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(data),
+            signal: controller.signal
         });
+
+        clearTimeout(timeoutId);
 
         if (!response.ok) {
             throw new Error('Erro de requisição');
